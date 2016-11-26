d6b2247de47af56c0fc5f1c08ac9e63c9747fc40
Michael Jaison G <gmichaeljaison@gmail.com>
2016-11-26 14:28:17 -0500

    quick-start sphinx documentation created

---
+++ _build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to instance-recognition&#8217;s documentation!"],titleterms:{document:0,indice:0,instanc:0,recognit:0,tabl:0,welcom:0}})
